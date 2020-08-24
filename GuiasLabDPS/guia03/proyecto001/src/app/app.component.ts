e652ca482d03b16afe855167bb83c17493a7b948
mariobeltran <mariobeltran1999@gmail.com>
2020-08-23 22:07:18 -0600

    Actualizando Proyecto001

--- GuiasLabDPS/guia03/proyecto001/src/app/app.component.ts
+++ GuiasLabDPS/guia03/proyecto001/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getMaxListeners } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto001';
+  nombre:string = 'Pablo Rodríguez';
+  edad:number = 18;
+  email:string = 'rpablo@gmail.com';
+  sueldo:Array<number> =[1700,1600,1500];
+  activo:boolean = true;
+  sitio:string = 'https://www.google.com';
+  contador:number = 1;
+
+  incrementar(){
+    this.contador++;
+  }
+  decrementar(){
+    this.contador--;
+  }
+
+  esActivo(){
+    if(this.activo){
+      return 'Trabajador Activo'
+    }else{
+      return 'Trabajador Inactivo'
+    }
+  }
+  ultimos3sueldos(){
+    let suma = 0;
+    for(let x=0;x<this.sueldo.length;x++){
+      suma+=this.sueldo[x];
+    }
+    return suma;
+  }
 }
