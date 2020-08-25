ea016174aa43bfe566fe62ce90b87f2a02c2a06a
mariobeltran <mariobeltran1999@gmail.com>
2020-08-24 23:10:27 -0600

    Cambios Discusion v1.0.0

--- GuiasLabDPS/guia03/Discusion/Sueldos/src/app/app.module.ts
+++ GuiasLabDPS/guia03/Discusion/Sueldos/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { MaterialModule } from './material/material.module';
+import { ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
