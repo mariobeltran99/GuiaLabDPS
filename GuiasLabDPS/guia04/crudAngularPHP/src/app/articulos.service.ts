a6b0b3d357dd772b82f377d5d7207adefd1a38ea
mariobeltran <mariobeltran1999@gmail.com>
2020-09-06 13:02:49 -0600

    Ejercicio2 Finalizado

--- GuiasLabDPS/guia04/crudAngularPHP/src/app/articulos.service.ts
+++ GuiasLabDPS/guia04/crudAngularPHP/src/app/articulos.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticulosService {
 
-  constructor() { }
+  url = "https://mecanictools.000webhostapp.com/backend/";
+  constructor(private http:HttpClient) {}
+
+  recuperarTodos(){
+    return this.http.get(`${this.url}recuperartodos.php`);
+  }
+  alta(articulo){
+    return this.http.post(`${this.url}alta.php`,JSON.stringify(articulo));
+  }
+  baja(codigo:number){
+    return this.http.get(`${this.url}baja.php?codigo=${codigo}`);
+  }
+  seleccionar(codigo:number){
+    return this.http.get(`${this.url}seleccionar.php?codigo=${codigo}`);
+  }
+  modificacion(articulo){
+    return this.http.post(`${this.url}modificacion.php`,JSON.stringify(articulo));
+  }
 }
