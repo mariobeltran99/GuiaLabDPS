ac0d26fad34fe405c484568faebc78402b5e9fc9
mariobeltran99 <mariobeltran1999@gmail.com>
2020-09-11 23:09:37 -0600

    crudAngularFirebase

---
+++ GuiasLabDPS/guia05/crudAngularFirebase/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = { 
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDopSA-Py58N6oGqa7_dh8WpDm0_fG6uqg",
+    authDomain: "crudangularfirebase-bb723.firebaseapp.com",
+    databaseURL: "https://crudangularfirebase-bb723.firebaseio.com",
+    projectId: "crudangularfirebase-bb723",
+    storageBucket: "crudangularfirebase-bb723.appspot.com",
+    messagingSenderId: "622016335323",
+    appId: "1:622016335323:web:3aae086673a048e312abfe",
+    measurementId: "G-1NRYS4C802"
+  }
+};
---
+++ GuiasLabDPS/guia05/crudAngularFirebase/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDopSA-Py58N6oGqa7_dh8WpDm0_fG6uqg",
+    authDomain: "crudangularfirebase-bb723.firebaseapp.com",
+    databaseURL: "https://crudangularfirebase-bb723.firebaseio.com",
+    projectId: "crudangularfirebase-bb723",
+    storageBucket: "crudangularfirebase-bb723.appspot.com",
+    messagingSenderId: "622016335323",
+    appId: "1:622016335323:web:3aae086673a048e312abfe",
+    measurementId: "G-1NRYS4C802"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
